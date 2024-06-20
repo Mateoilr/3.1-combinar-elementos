84f373e9fb77bc1616868d8e90aa6dfa7bc40029
Mateo Lituma <milituma@sudamericano.edu.ec>
2024-06-20 02:17:30 -0500

    ejercice

---
+++ combinar-elementos.js
@@ -0,0 +1,25 @@
+'use strict';
+//Ejercicio 2
+function mostrarCombinaciones(nums) {
+  for (let i of nums) {
+    for (let j of nums) {
+      alert(i + "" + j);
+    }
+  }
+}
+
+let nums = [0, 2, 4, 8];
+mostrarCombinaciones(nums);
+
+//Ejercicio 3
+
+function mostrarCombinacionesSinRepetir(nums2) {
+  for (let i = 0; i < nums2.length; i++) {
+    for (let j = i + 1; j < nums2.length; j++) {
+      alert(nums2[i] + "" + nums2[j]);
+    }
+  }
+}
+
+let nums2 = [0, 2, 4, 8];
+mostrarCombinacionesSinRepetir(nums2);
